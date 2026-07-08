cbac5644e5c0abd279f7863ae892f05a0ce9056d
Garrett Girod <girodg@hotmail.com>
2026-07-08 11:56:26 -0400

    linear search

--- Week2/Lectures/Searcher.cpp
+++ Week2/Lectures/Searcher.cpp
@@ -3,3 +3,17 @@
 //
 // Part B-1
 //
+
+int Searcher::LinearSearch(const std::vector<Light>& lights, int greenToFind) const
+{
+	int index = NOT_FOUND;
+	for (int i = 0; i < lights.size(); i++)
+	{
+		if (greenToFind == lights[i].green)
+		{
+			index = i;
+			break;
+		}
+	}
+    return index;
+}
--- Week2/Lectures/Searcher.h
+++ Week2/Lectures/Searcher.h
@@ -2,11 +2,20 @@
 #include <vector>
 #include "Light.h"
 
+const int NOT_FOUND = -1;
+
 class Searcher
 {
 public:
 	//
 	// Part B-1
 	//
+	/// <summary>
+	/// Searches the vector using the linear search algorithm
+	/// </summary>
+	/// <param name="lights">the vector to search</param>
+	/// <param name="greenToFind">the value to find</param>
+	/// <returns>-1 if not found, the index if found</returns>
+	int LinearSearch(const std::vector<Light>& lights, int greenToFind) const;
 };
 
