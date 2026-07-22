1c11b3c7b5fadb7d6c6587ec677b70aee3a2c7c2
Garrett Girod <girodg@hotmail.com>
2026-07-22 09:59:09 -0400

    writing CSV

--- Week4/Lectures/Lectures.cpp
+++ Week4/Lectures/Lectures.cpp
@@ -3,10 +3,55 @@
 #include "Day10.h"
 #include "Day11.h"
 #include "Input.h"
+#include <fstream>
 
+//
+//3 basic steps for File I/O
+//1) open the file
+//		where is the file?
+//		what mode to open it in? (text, binary, read, write, both, append)
+//2) read/write to the file
+//3) CLOSE the file
+//		good programming practice: ASAP. 
 
 int main(int argc, char* args[])
 {
+	// Write a CSV file
+	//
+	// 1) open the file
+	//	  where the file is?
+	//		path to the file
+	//		- full paths
+	//		- relative paths
+	std::string path = "C:/temp/2607/";
+	std::string fileName = "2607.csv";
+	std::string finalPath = path + fileName;
+
+	//write a csv file
+	//use the ofstream class
+	//  'o' stands for output (writing)
+	// creates the file if it does not exists
+	// overwrite if the file does exists
+	// it will NOT create the path.
+	//	the path MUST exists before trying to open the file.
+	//1) open the file
+	//		MUST check if it worked
+	std::ofstream outFile(finalPath);
+	char delimiter = '?';
+	if (outFile.is_open())
+	{
+		//2) write to the file
+		outFile << "Batman is the GOAT!\n";
+		outFile << 4.2 << delimiter << true << delimiter << 13 << "\n";
+		outFile << "Aquaman smells.";
+	}
+	else
+	{
+		std::cout << finalPath << " could not be opened.\n";
+	}
+	//3) CLOSE the file
+	outFile.close();
+
     std::string hello = "Hello Week 4!";
     for (auto& ch : hello)
     {
